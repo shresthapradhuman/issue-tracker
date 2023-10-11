ed031e8cfbeb378a7746508ce9c7154b124e65df
shrestha@veriteworks.co.jp <shrestha@veriteworks.co.jp>
2023-10-11 12:06:17 +0900

    Build a styled link component

---
+++ app/components/Link.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NextLink from "next/link";
+import { Link as RadixLink } from "@radix-ui/themes";
+
+const Link = ({ href, children }: { href: string; children: string }) => {
+  return (
+    <NextLink href={href}>
+      <RadixLink>{children}</RadixLink>
+    </NextLink>
+  );
+};
+
+export default Link;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import prisma from "@/prisma/client";
 import IssueStatusBage from "../components/IssueStatusBage";
 import delay from "delay";
 import IssueActions from "./IssueActions";
-import Link from "next/link";
+import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
