fba350bf2177d7503a6f0204a3785c9b0cb79f1a
shrestha@veriteworks.co.jp <shrestha@veriteworks.co.jp>
2023-10-11 17:49:21 +0900

    Authentication Fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,6 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/prisma/client";
+import NextAuth from "next-auth";
+import { authOptions } from "../authOptions";
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId:
-        "380067373305-jp3lahffmp5vua8ecjjjgsmi1l34dp5m.apps.googleusercontent.com",
-      clientSecret: "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",
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
---
+++ app/api/auth/authOptions.ts
@@ -0,0 +1,20 @@
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      httpOptions: {
+        timeout: 40000,
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
