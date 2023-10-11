fd49ad484a502125967909aab1cbfef8a62f19df
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-10-11 23:43:07 +0900

    Refactor AssigneeSelect component

--- app/issues/[id]/AssignmentSelect.tsx
+++ app/issues/[id]/AssignmentSelect.tsx
@@ -1,22 +1,13 @@
 "use client";
+import Skeleton from "react-loading-skeleton";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import Skeleton from "react-loading-skeleton";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssignmentSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
   if (isLoading) return <Skeleton />;
   if (error) return null;
   return (
@@ -48,5 +39,14 @@ const AssignmentSelect = ({ issue }: { issue: Issue }) => {
       <Toaster />
     </>
   );
+};
+
+const useUsers = () => {
+  return useQuery<User[]>({
+    queryKey: ["users2"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 };
 export default AssignmentSelect;
