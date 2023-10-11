0ce8f769e9aaccc910fd84dc4c6607272e3439bf
shrestha@veriteworks.co.jp <shrestha@veriteworks.co.jp>
2023-10-11 18:14:57 +0900

    Build the assignee select component

---
+++ app/issues/[id]/AssignmentSelect.tsx
@@ -0,0 +1,18 @@
+import { Select } from "@radix-ui/themes";
+import React from "react";
+
+const AssignmentSelect = () => {
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="1">Pradhuman Shrestha</Select.Item>
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default AssignmentSelect;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,6 +7,7 @@ import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/authOptions";
+import AssignmentSelect from "./AssignmentSelect";
 
 interface Props {
   params: { id: string };
@@ -29,6 +30,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction={"column"} gap={"4"}>
+            <AssignmentSelect />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
